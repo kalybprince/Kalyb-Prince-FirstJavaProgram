8f4c813ea6d7ceeabd884f3f58240b8d2ba5050a
Kalyb Prince <kalybprince91@gmail.com>
2021-09-24 20:23:25 -1000

    Init project

---
+++ AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Kalyb Prince");
+        System.out.println("I am 1000 years old");
+        System.out.println("My hometown is Lawson, Missouri");
+    }
+}
